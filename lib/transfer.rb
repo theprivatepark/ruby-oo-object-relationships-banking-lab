89b80a708535978fa2d69604b55ef820bcd5bd34
theprivatepark <anneypark@gmail.com>
2020-08-28 18:21:14 -0400

    complete bank account transfer

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,9 +16,24 @@ class Transfer
   end
 
   def execute_transaction
-    self.sender.balance -= @amount
-    self.receiver.balance += @amount
-    self.status = "complete"
+    if self.valid? && self.status == "pending" && self.sender.balance >= self.amount
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer 
+    #sender is taking back the amount that was sent to the receiver, receivers amount will decrease and senders amount will increase
+    #sender is taking back @amount and adding it to senders account/balance , receiver is losing @amount in their account/balance 
+    if self.status == "complete"
+        self.sender.balance += self.amount
+        self.receiver.balance -= self.amount
+        self.status = "reversed"
+    end
   end
 
 end
