fe4a3029393b28ff1b04c3114affbdd8d515253f
theprivatepark <anneypark@gmail.com>
2020-08-28 15:14:22 -0400

    bank account updated

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+    attr_reader :name
+    attr_accessor  :balance,  :status, :deposit
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(balance)
+        @balance += balance
+    end
+
+    def display_balance
+        return "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+         self.status == "open" && self.balance > 0
+    end
+    
+    def close_account
+        self.status = "closed"
+    end
 end
